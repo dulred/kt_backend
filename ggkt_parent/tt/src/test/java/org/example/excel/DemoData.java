feb595db105387ae8045626c3dbb0de5add6f540
cccom <cc@qq.com>
2023-08-14 02:14:47 +0800

    third commit

---
+++ ggkt_parent/tt/src/test/java/org/example/excel/DemoData.java
@@ -0,0 +1,22 @@
+package org.example.excel;
+
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
+
+import java.util.Date;
+
+
+public class DemoData {
+
+    @ExcelProperty("title")
+    private String name;
+    @ExcelProperty("date")
+    private Date date;
+    @ExcelProperty("numberTitle")
+    private double doubleValule;
+
+    @ExcelIgnore
+    private String ignore;
+
+
+}
---
+++ ggkt_parent/tt/src/test/java/org/example/excel/ShallowTest.java
@@ -0,0 +1,18 @@
+package org.example.excel;
+
+public class ShallowTest {
+
+
+    public static void main(String[] args) {
+
+        testData();
+
+    }
+
+    private static void testData() {
+
+        DemoData data = new DemoData();
+
+
+    }
+}
